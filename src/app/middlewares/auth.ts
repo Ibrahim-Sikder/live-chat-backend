dad0fe74d4d6af9aa14cec46acfb0de07147dd9f
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-09-06 13:00:20 +0600

    chat-route-done

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,34 +1,39 @@
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
 import { User } from '../modules/user/user.model';
 import config from '../config';
+
+// Extend the JwtPayload type to include `id` if it's part of your payload
+interface CustomJwtPayload extends JwtPayload {
+  id: string;
+}
 
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      token = req.headers.authorization.split(" ")[1];
-   
-      //decodes token id
-      const decoded = jwt.verify(token, config.jwt_access_secret as string);
+      token = req.headers.authorization.split(' ')[1];
+      console.log(token);
+      // Decodes token and casts it to the custom payload type
+      const decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as CustomJwtPayload;
+      console.log(decoded.id);
 
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.id).select('-password');
 
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("Not authorized, token failed");
+      throw new Error('Not authorized, token failed');
     }
-  }
-
-  if (!token) {
+  } else {
     res.status(401);
-    throw new Error("Not authorized, no token");
+    throw new Error('Not authorized, no token');
   }
 });
-
-
